600f76651da3f554a7cf2ccd3725c8929909e374
victor aguilar moreno <victoraguilarmoreno@gmail.com>
2017-06-26 19:43:07 +0200

    tercera subida web

--- consumepostsmongo.js
+++ consumepostsmongo.js
@@ -6,7 +6,7 @@ var response;
 function obtenerPosts(){
 //declaramos la peticion , eto sirve par ahacer la peticion a la url y necesito (ya que es asincrona)ç
 // que quiero que haga cuando traiga los datos , tercerparametro false forma sincrono
- var peticion = new XMLHttpRequest();
+  var peticion = new XMLHttpRequest();
  peticion.open("GET",URL,false);
  //quiero que me traigas en formato json, si se puede
  peticion.setRequestHeader("Content-Type","application/json");
@@ -17,21 +17,64 @@ function obtenerPosts(){
  //}
  //como le he puesto false sincrono debera enviarme la respesta
  response = JSON.parse(peticion.responseText);
+ // para que cuando tenga un id concreto
+ sessionStorage["posts"]=peticion.responseText;
  console.log(response);
- mostrarPosts();
+ //mostrarPosts();
 };
 function mostrarPosts(){
+var peticion = new XMLHttpRequest();
 var tabla = document.getElementById("tablaPosts");
   for (var i = 0; i < response.length; i++) {
   ///  alert(response[i].titulo);
       var fila = tabla.insertRow(i+1);
-      var celdaTitulo = fila.insertCell(0);
-      var celdaTexto = fila.insertCell(1);
-      var celdaAutor = fila.insertCell(2);
+      var celdaId = fila.insertCell(0)
+      var celdaTitulo = fila.insertCell(1);
+      var celdaTexto = fila.insertCell(2);
+      var celdaAutor = fila.insertCell(3);
+      // metemos los botones en la tabla
+      var celdaOperaciones= fila.insertCell(4);
+      celdaId.innerHTML = response[i]._id.$oid;
       celdaTitulo.innerHTML = response[i].titulo;
   //    if(response [i].autor != undefined() {
       celdaAutor.innerHTML = response[i].autor;
       celdaTexto.innerHTML = response[i].texto;
-
+      celdaOperaciones.innerHTML = '<button onclick=\'actualizarPosts("' +
+       celdaId.innerHTML + '")\';>Actualizar</button>';
   }
+}
+function anadirPosts(){
+    var peticion = new XMLHttpRequest();
+    peticion.open("POST",URL,false);
+    peticion.setRequestHeader("Content-Type","application/json");
+    peticion.send('{"titulo":"Nuevo POST desde victor", "texto":"Nuevo texto desde ATOM", "autor":{"nombre":"Fernando", "apellido":"Minguero"}}');
+}
+function actualizarPosts(id){
+        var peticion = new XMLHttpRequest();
+        var URLItem =  "https://api.mlab.com/api/1/databases/bootcamp/collections/posts/";
+        URLItem += id;
+        var APIKey = "?apiKey=fQJ18loW9DbBacFeit4xr1biROFiTxel";
+        URLItem += APIKey;
+        //usamos el put poro con otra url
+        peticion.open("PUT",URLItem,false);
+        peticion.setRequestHeader("Content-Type","application/json");
+        peticion.send(' {"titulo":"hago PUT en el registro"}');
+
+}
+function seleccionarPosts(numero){
+      sessionStorage["seleccionado"]=numero;
+
+}
+function buscarDetallesPost(numero) {
+    var posts=JSON.parse(sessionStorage["posts"]);
+    for (var i=0; i< posts.length ;i++) {
+        if (posts[i]._id.$oid == numero)
+       {
+         //mostrar detalle
+         document.getElementById("h1").innerHTML =numero;
+         document.getElementById("h2").innerHTML =posts[i].titulo;
+         document.getElementById("h3").innerHTML =posts[i].texto;
+         break;
+       }
+    }
 }
--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const express =require('express');
 const path =require('path');
 //Trae las funciones de buscar los directorios
 //Constantes
-const PORT = 8081;
+const PORT = 8082;
 //app --con esto inicioalizo el servidor , ya esta escuchando en el 8080
-<<<<<<< HEAD
-
 const app =express();
 app.use(express.static(__dirname));
-=======
-const app =express();
->>>>>>> 5776a1fc5efd9fa04fd7eab24fef9da6a6605796
 // ponenos los response y request, el propio get si resuelve la informacion del navegador y podria enviar una cockie
-app.get('/',function(req,res){
+app.get('/',function(request,respuesta){
 //  res.send("Bienvenido a BBVA techUniversity&reg;\n") ahora para poner la web neecsito indicar donde esta el directiro
 // busco el directorio y los uno con join para qu haga un solo path
-  res.sendfile(path.join(__dirname + '/index.html'));
+  respuesta.sendfile(path.join(__dirname + '/index.html'));
+});
+app.get('/detallePost/:id',function(request,respuesta){
+//  servira para poder enviar el detallePost y enviar otra pagina pero con el codigo del detalle
+  respuesta.sendfile(path.join(__dirname + '/detallePost.html'));
 });
 //para asegurame que funciona escucho
 app.listen(PORT);
